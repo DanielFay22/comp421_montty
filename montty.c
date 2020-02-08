ee9a43d7d597169a97f2878762d8ecb2eeef78fa
DanielFay22 <dpf2@rice.edu>
2020-02-08 15:58:12 -0600

    Adds some more stuff

--- montty.c
+++ montty.c
@@ -4,6 +4,7 @@
 #include "terminals.h"
 #include "threads.h"
 
+#include "monitor.c"
 
 int input_char(int term);
 int write_chars(int term, char *buf, int buflen);
@@ -39,4 +40,28 @@ int TerminalDriverStatistics(struct termstat *stats) {
 
 int InitTerminalDriver(void) {
     init_monitor();
+}
+
+
+void user_thread(int *term) {
+    InitTerminal(*term);
+
+    char *buf = (char *) (malloc(sizeof(char) * BUF_LEN));
+    int read;
+
+    while (1) {
+        read = ReadTerminal(*term, buf, BUF_LEN);
+        WriteTerminal(*term, buf, read);
+    }
+}
+
+
+int main(void) {
+
+    InitTerminalDriver();
+
+    for (int i = 0; i < NUM_TERMINALS; i ++)
+        ThreadCreate(user_thread, &i)
+
+    ThreadWaitAll();
 }
