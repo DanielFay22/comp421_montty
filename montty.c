87f43a2bbeef4c1df49d9f2a811d7798399e13bb
DanielFay22 <dpf2@rice.edu>
2020-02-08 15:38:20 -0600

    Added some stuff that may or may not work

--- montty.c
+++ montty.c
@@ -5,36 +5,38 @@
 #include "threads.h"
 
 
-
+int input_char(int term);
+int write_chars(int term, char *buf, int buflen);
+int read_chars(int term, char *buf, int buflen);
+void set_data_register_ready(int term);
+int get_stats(struct termstat *cpy_stats);
+void init_monitor();
 
 
 void TransmitInterrupt(int term) {
-
+    set_data_register_ready(term);
 }
-
 
 void ReceiveInterrupt(int term) {
-
+    input_char(term);
 }
 
-
 int WriteTerminal(int term, char *buf, int buflen) {
-
+    return write_chars(term, buf, buflen);
 }
 
 int ReadTerminal(int term, char *buf, int buflen) {
-
+    return read_chars(term, buf, buflen);
 }
 
-
 int InitTerminal(int term) {
-
+    return InitHardware(term);
 }
 
 int TerminalDriverStatistics(struct termstat *stats) {
-
+    return get_stats(stats);
 }
 
 int InitTerminalDriver(void) {
-
+    init_monitor();
 }
