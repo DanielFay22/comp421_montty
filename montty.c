f10515096509b9e45d1eb8601649568a63f802e1
DanielFay22 <dpf2@rice.edu>
2020-02-08 18:51:21 -0600

    debugging

--- montty.c
+++ montty.c
@@ -59,6 +59,7 @@ extern void ReceiveInterrupt(int term) {
     if (input_chars[term] == BUF_LEN || echo_chars[term] == BUF_LEN)
         return;
 
+    printf("Input chars buffer: %d\n", input_chars[term]);
 
     switch (c){
         case '\b':
@@ -80,7 +81,7 @@ extern void ReceiveInterrupt(int term) {
         case '\n':
             input_buffer[term][input_write_pos[term]] = '\n';
             input_write_pos[term] = (input_write_pos[term] + 1) % BUF_LEN;
-            input_chars[term]++;
+            ++input_chars[term];
 
             echo(term, "\r\n", 2);
 
@@ -94,8 +95,8 @@ extern void ReceiveInterrupt(int term) {
             echo_buffer[term][echo_write_pos[term]] = c;
             echo_write_pos[term] = (echo_write_pos[term] + 1) % BUF_LEN;
 
-            input_chars[term]++;
-            echo_chars[term]++;
+            ++input_chars[term];
+            ++echo_chars[term];
 
             break;
 
